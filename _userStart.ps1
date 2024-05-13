7065ac9140ecfe4b3a2f6f82f757c02b1bba9376
appstreamqs <164137716+appstreamqs@users.noreply.github.com>
2024-05-13 13:58:37 +0530

    Update _userStart.ps1

--- _userStart.ps1
+++ _userStart.ps1
@@ -1,4 +1,4 @@
-$url = "https://gist.githubusercontent.com/appstreamqs/1cb1ea7b67eb9e6ee662351327371d80/raw/3ef9f8b72729cd965278603aea3be80ce29fe416/user_presession.ps1"
+$url = "https://gist.githubusercontent.com/appstreamqs/1cb1ea7b67eb9e6ee662351327371d80/raw/user_presession.ps1"
 $outpath = "C:\Users\Public\user_presession.ps1"
 Invoke-WebRequest -Uri $url -OutFile $outpath
 Write-Output "_userStart.ps1"
